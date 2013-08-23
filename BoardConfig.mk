f294dd3b95ba4ee533f5af814c82e59f03f6a67c
nuclearmistake <nuclearmistake@gmail.com>
2013-08-23 12:48:20 -0400

    Use 4.6 to compile hercules kernel.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,8 @@ TARGET_OTA_ASSERT_DEVICE := SGH-T989,hercules
 TARGET_KERNEL_CONFIG        := cyanogenmod_hercules_defconfig
 TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
 
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
+
 # Assert minimum baseband version
 TARGET_BOARD_INFO_FILE ?= device/samsung/hercules/board-info.txt
 
